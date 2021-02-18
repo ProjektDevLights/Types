6360f24e8f38e7d761f264e2c85c125787da39b4
Timo Peters <peters@teck-digital.de>
2021-02-18 10:07:37 +0100

    added export for response

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
-export {default as Light} from "./src/Light";
-export {default as Alarm} from "./src/Alarm";
-export {Pattern} from "./src/Pattern/Pattern";
-export {default as PartialLight} from "./src/PartialLight";
-export {UserPattern} from "./src/Pattern/user-pattern";
-export {FakePattern} from "./src/Pattern/fake-pattern";
+export { default as Alarm } from "./src/Alarm";
+export { default as Light } from "./src/Light";
+export { default as PartialLight } from "./src/PartialLight";
+export { FakePattern } from "./src/Pattern/fake-pattern";
+export { Pattern } from "./src/Pattern/Pattern";
+export { UserPattern } from "./src/Pattern/user-pattern";
+export { default as Response } from "./src/Response";
