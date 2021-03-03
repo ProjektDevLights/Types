63c26b25cab5480e152dbd4a14fd39f241835226
Jozys <slaar@teckdigital.de>
2021-03-03 17:25:10 +0100

    Added timeout for Leds + Fixed exports

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
 export { default as Alarm } from "./src/Alarm";
 export { default as Light } from "./src/Light";
+export { default as Leds } from "./src/Leds";
 export { default as PartialLight } from "./src/PartialLight";
-export { FakePattern } from "./src/Pattern/fake-pattern";
+export { FakePattern, FAKE_PATTERNS } from "./src/Pattern/fake-pattern";
 export { Pattern } from "./src/Pattern/Pattern";
-export { UserPattern } from "./src/Pattern/user-pattern";
+export { UserPattern, USER_PATTERNS } from "./src/Pattern/user-pattern";
 export { default as Response } from "./src/Response";
--- src/Leds.ts
+++ src/Leds.ts
@@ -3,4 +3,5 @@ import { Pattern } from "./Pattern/Pattern";
 export default interface Leds {
     colors: string[];
     pattern: Pattern;
+    timeout?: number;
   }
