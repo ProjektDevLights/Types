b605b02786867a0be266fa536ccae275a498dd71
Jozys <slaar@teckdigital.de>
2021-03-08 18:21:27 +0100

    Updated Alarm Types + Added Runner Pattern
    Fixed #1 + Fixed #2

--- src/Alarm.ts
+++ src/Alarm.ts
@@ -1,7 +1,10 @@
+import PartialLight from "./PartialLight";
+
 export default interface Alarm{
+    id: string,
     color: string,
     date: string,
     days: number[],
     repeat: number,
-    esps: string[], // ids
+    lights: PartialLight[],
 }
