2e8fbecb9a8ec658ff271133a947032365ca49e6
Jozys <slaar@teckdigital.de>
2021-04-09 11:51:51 +0200

    Updated Pattern types

--- src/Pattern/fake-pattern.ts
+++ src/Pattern/fake-pattern.ts
@@ -1,2 +1,2 @@
-export const FAKE_PATTERNS = ["waking", "blinking", "fading"] as const;
+export const FAKE_PATTERNS = ["waking", "blinking", "fading", "custom"] as const;
 export type FakePattern = (typeof FAKE_PATTERNS)[number];
--- src/Pattern/user-pattern.ts
+++ src/Pattern/user-pattern.ts
@@ -1,2 +1,2 @@
-export const USER_PATTERNS = ["plain", "gradient", "runner"] as const;
+export const USER_PATTERNS = ["plain", "gradient", "runner", "rainbow"] as const;
 export type UserPattern = (typeof USER_PATTERNS)[number];
