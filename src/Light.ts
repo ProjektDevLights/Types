6799e1a4d0379fbf6af34b90914afa1baeba8dfd
Timo Peters <peters@teck-digital.de>
2021-06-27 20:05:37 +0200

    added position to light type

--- src/Light.ts
+++ src/Light.ts
@@ -1,11 +1,12 @@
 import Leds from "./Leds";
 
 export default interface Light {
-    name: string;
-    id: string;
-    leds: Leds;
-    count: number;
-    brightness: number;
-    tags?: string[];
-    isOn: boolean;    
+  name: string;
+  id: string;
+  leds: Leds;
+  count: number;
+  brightness: number;
+  tags?: string[];
+  isOn: boolean;
+  position: number;
 }
