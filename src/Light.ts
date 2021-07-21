bfd38b1900234db19aa5c0124e4bcf71d52f477b
Jozys <slaar@teckdigital.de>
2021-07-21 20:58:26 +0200

    added custom_sequence to light

--- src/Light.ts
+++ src/Light.ts
@@ -1,11 +1,14 @@
+import CustomData from "./CustomData";
 import Leds from "./Leds";
 
 export default interface Light {
-    name: string;
-    id: string;
-    leds: Leds;
-    count: number;
-    brightness: number;
-    tags?: string[];
-    isOn: boolean;    
+  name: string;
+  id: string;
+  leds: Leds;
+  count: number;
+  brightness: number;
+  tags?: string[];
+  isOn: boolean;
+  custom_sequence?: CustomData[];
+  comment?: string;
 }
