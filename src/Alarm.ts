8713923b1812659ad749a90eabeb6fe8a830a588
Jozys <slaar@teckdigital.de>
2022-03-14 13:01:40 +0100

    Added custom sequence & leds to Alarm type

--- src/Alarm.ts
+++ src/Alarm.ts
@@ -1,10 +1,13 @@
+import CustomData from "./CustomData";
+import Leds from "./Leds";
 import PartialLight from "./PartialLight";
 
 export default interface Alarm {
   name?: string;
   isOn: boolean;
   id: string;
-  color: string;
+  leds: Omit<Leds, "timeout">;
+  custom_sequence?: CustomData[];
   time: string;
   days: number[];
   lights: PartialLight[];
